6f1fb3e14a2b79a4d4548d5415045dbb0e2b15d3
aguilerad916 <aguilerad916@gmail.com>
2023-08-16 22:30:28 -0700

    .

---
+++ components/AboutSection.tsx
@@ -0,0 +1,79 @@
+import React from "react";
+import Image from "next/image";
+
+const skills = [
+  { skill: "HTML" },
+  { skill: "CSS" },
+  { skill: "JavaScript" },
+  { skill: "TypeScript" },
+  { skill: "Python" },
+  { skill: "React" },
+  { skill: "Next.js" },
+  { skill: "Tailwind CSS" },
+  { skill: "Git" },
+  { skill: "SQL" },
+  { skill: "Prisma" },
+  { skill: "Node.js" },
+  { skill: "Express" },
+  { skill: "Vercel" },
+];
+
+const AboutSection = () => {
+  return (
+    <section id="about">
+      <div className="my-12 pb-12 md:pt-16 md:pb-48">
+        <h1 className="text-center font-bold text-4xl">
+          About Me
+          <hr className="w-6 h-1 mx-auto my-4 bg-teal-500 border-0 rounded"></hr>
+        </h1>
+
+        <div className="flex flex-col space-y-10 items-stretch justify-center align-top md:space-x-10 md:space-y-0 md:p-4 md:flex-row md:text-left">
+          <div className="md:w-1/2 ">
+            <p>
+              Hey, my name is Daniel and I am a
+              <span className="font-bold">{" open-minded"}</span>,
+              <span className="font-bold">{" motivated"}</span>, and
+              <span className="font-bold">{" adaptable"}</span> software
+              engineer based in Los Angeles, CA.
+            </p>
+            <br />
+            <p>
+              I am a self-taught developer and have been coding since I was for
+              over a year now. I am highly motivated and eager to apply my
+              skills to real-world to help contribute to.
+            </p>
+            <br />
+            <p>
+              My hobbies include reading, playing any sports, gaming, and
+              traveling to new places. I am always looking for new experiences
+              to help me grow as a person and as a developer.
+            </p>
+            <br />
+            <p>
+              I personally believe that the best way to learn is by doing. You
+              can always become a better version of yourself. I am always
+              striving to learn new things and expand my knowledge.
+            </p>
+          </div>
+          <div className="text-center md:w-1/2 md:text-left">
+            <h1 className="text-2xl font-bold mb-6">My Skills</h1>
+            <div className="flex flex-wrap flex-row justify-center z-10 md:justify-start">
+              {skills.map((item, idx) => {
+                return (
+                  <p
+                    key={idx}
+                    className="bg-gray-200 px-4 py-2 mr-2 mt-2 text-gray-500 rounded font-semibold"
+                  >
+                    {item.skill}
+                  </p>
+                );
+              })}
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default AboutSection;
---
+++ components/ProjectsSection.tsx
@@ -0,0 +1,88 @@
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+import SlideUp from "./SlideUp";
+import { BsGithub, BsArrowUpRightSquare } from "react-icons/bs";
+
+const projects = [
+  {
+    name: "Internet Speed Test",
+    description: "Web App that tests your internet speed.",
+    image: "/internet-speed.png",
+    github: "https://github.com/aguilerad916/internet-speed",
+    link: "https://internet-speed.vercel.app/",
+  },
+  {
+    name: "WeatherWise",
+    description:
+      "WeatherWise is a weather app that allows users to search for the current weather in any city in the world.",
+    image: "",
+    github: "https://github.com/aguilerad916/weather-app",
+    link: "https://weather-app-git-main-aguilerad916.vercel.app/",
+  },
+  {
+    name: "Twitter Clone",
+    description:
+      "Twitter Clone is a social media app that allows users to post tweets, follow other users, and like tweets using the T3 Stack.",
+    image: "/twitter-clone.png",
+    github: "https://github.com/aguilerad916/twitter-clone",
+    link: "https://twitter-clone-roan-mu-11.vercel.app/",
+  },
+];
+
+const ProjectsSection = () => {
+  return (
+    <section id="projects">
+      <h1 className="my-10 text-center font-bold text-4xl">
+        Projects
+        <hr className="w-6 h-1 mx-auto my-4 bg-teal-500 border-0 rounded"></hr>
+      </h1>
+
+      <div className="flex flex-col space-y-28">
+        {projects.map((project, idx) => {
+          return (
+            <div key={idx}>
+              <SlideUp offset="-300px 0px -300px 0px">
+                <div className="flex flex-col  animate-slideUpCubiBezier animation-delay-2 md:flex-row md:space-x-12">
+                  <div className=" md:w-1/2">
+                    <Link href={project.link}>
+                      <Image
+                        src={project.image}
+                        alt=""
+                        width={1000}
+                        height={1000}
+                        className="rounded-xl shadow-xl hover:opacity-70"
+                      />
+                    </Link>
+                  </div>
+                  <div className="mt-8 md:w-1/2">
+                    <h1 className="text-4xl font-bold mb-6">{project.name}</h1>
+                    <p className="text-xl leading-7 mb-4 text-neutral-600 dark:text-neutral-400">
+                      {project.description}
+                    </p>
+                    <div className="flex flex-row align-bottom space-x-4">
+                      <Link href={project.github} target="_blank">
+                        <BsGithub
+                          size={30}
+                          className="hover:-translate-y-1 transition-transform cursor-pointer"
+                        />
+                      </Link>
+                      <Link href={project.link} target="_blank">
+                        <BsArrowUpRightSquare
+                          size={30}
+                          className="hover:-translate-y-1 transition-transform cursor-pointer"
+                        />
+                      </Link>
+                    </div>
+                  </div>
+                </div>
+              </SlideUp>
+            </div>
+          );
+        })}
+      </div>
+    </section>
+  );
+};
+
+export default ProjectsSection;
